e16635b267c0328c07fc8ac0e83f82749b26408f
Seyo006 <lina.chebel06150@gmail.com>
2025-11-13 11:58:32 +0100

    Vérfication phrase info

--- data/main.go
+++ data/main.go
@@ -202,7 +202,7 @@ var quizzes = map[string]Quiz{
 			{
 				Text: "Quel est l’objectif principal de l’apprentissage supervisé ?",
 				Choices: []string{
-					"Générer du cod aléatoire",
+					"Générer du code aléatoire",
 					"Apprendre à partir de données étiquetées pour prédire ou classer",
 					"Supprimer des données inutiles",
 				},
