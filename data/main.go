de4f064d12dea4b0adbe91f0acf69945c835bef5
Ranya <ranya@MacBook-Pro-de-Ranya.local>
2025-11-13 12:00:32 +0100

    modif de texte vocab cyber

--- data/main.go
+++ data/main.go
@@ -31,7 +31,7 @@ var quizzes = map[string]Quiz{
 				CorrectIndex: 2,
 			},
 			{
-				Text:         "Quel langage est-il compilé ?",
+				Text:         "Quel langage est compilé ?",
 				Choices:      []string{"Python", "Go", "JavaScript"},
 				CorrectIndex: 1,
 			},
