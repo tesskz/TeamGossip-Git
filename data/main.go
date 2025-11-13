f20eefd40acedd09276490b8775e9e967b24bc21
Malek <malek@MacBook-Pro-de-Malek.local>
2025-11-13 11:47:25 +0100

    Question 8 complète

--- data/main.go
+++ data/main.go
@@ -242,7 +242,7 @@ var quizzes = map[string]Quiz{
 				CorrectIndex: 2,
 			},
 			{
-				Text: "",
+				Text: "Un fichier CSV sert en général à :",
 				Choices: []string{
 					"Stocker des images",
 					"Stocker des données tabulaires en texte brut",
